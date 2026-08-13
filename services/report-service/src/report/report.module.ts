0aaec567d5e3b74dd73299a15bd611f033bf6eb0
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2026-08-13 12:37:59 +0530

    fix(report-service) : fixed jwt module missing error

--- services/report-service/src/report/report.module.ts
+++ services/report-service/src/report/report.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles/roles.guard';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [ReportController],
   providers: [ReportService, JwtAuthGuard, RolesGuard],
 })
