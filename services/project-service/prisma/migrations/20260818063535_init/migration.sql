a8bd82a8dba84b08d73ce020a7044c6cac0cd14b
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2026-08-18 12:06:03 +0530

    init(project-service): created the prisma schema

---
+++ services/project-service/prisma/migrations/20260818063535_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "projects";
+
+-- CreateTable
+CREATE TABLE "projects"."Project" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
